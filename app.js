504b10e6c9521c115df462eb39e5de3e8eb5b02c
nar2p1nk <farrisisqandar@gmail.com>
2022-02-02 23:47:09 +0800

    made a function

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-var hello = 'hello world';
-var heads = document.createElement('h1');
-heads.textContent = hello;
-document.body.appendChild(heads);
+function hello(name, age, gender) {
+    if (gender == true) {
+        console.log("greetings " + name + ", wish you health at the fine age of " + age);
+    }
+    else {
+        console.log(name + "! you old crone nah jk i also wish you health AND wealth at the age of " + age);
+    }
+}
+hello('jakob', 30, true);
--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
-let hello:string = 'hello world';
+function hello(name:string,age:number,gender:boolean){
+    if(gender == true){
+        console.log(`greetings ${name}, wish you health at the fine age of ${age}`);
+    }
+    else{
+        console.log(`${name}! you old crone nah jk i also wish you health AND wealth at the age of ${age}`);
+        
+    }
+}
 
-let heads = document.createElement('h1')
-heads.textContent = hello;
-
-document.body.appendChild(heads);
